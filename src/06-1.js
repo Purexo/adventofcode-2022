8c449e0f0556b7391eae0e078c1ddd85293e5222
Purexo <contact@purexo.mom>
2022-12-06 10:46:31 +0100

    feat: D6 ⭐

---
+++ src/06-1.js
@@ -0,0 +1,17 @@
+import {createReadStream} from "node:fs";
+const chars = createReadStream(new URL('../fixtures/06.txt', import.meta.url), {highWaterMark: 1, encoding: 'utf8'});
+
+const last4Chars = [];
+let i = 0
+for await (const char of chars) {
+  const index = last4Chars.indexOf(char);
+
+  last4Chars.splice(0, index + 1);
+  last4Chars.push(char);
+
+  i++;
+  if (last4Chars.length === 4) break;
+}
+
+// 1909
+console.log(i);
